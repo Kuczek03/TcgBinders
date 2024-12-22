9a4ff4c2e6686e34e7fbcc0bc47cd295e7894025
Kuczek03 <thekolorowyyt@gmail.com>
2024-12-22 20:33:26 +0100

    Postgresql database connection and Card model created

--- TcgBinders/Data/ApplicationDbContext.cs
+++ TcgBinders/Data/ApplicationDbContext.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TcgBinders.Models;
 
 namespace TcgBinders.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
+
+    public DbSet<Card> Cards { get; set; }
 }
---
+++ TcgBinders/Models/Card.cs
@@ -0,0 +1,8 @@
+namespace TcgBinders.Models;
+
+public class Card
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Rarity { get; set; }
+}
