e0d50c941fe3530c5e2aaea28d716eac09c76df7
Kuczek03 <thekolorowyyt@gmail.com>
2024-12-31 01:51:53 +0100

    Set table and view created

---
+++ TcgBinders/Models/Set.cs
@@ -0,0 +1,9 @@
+namespace TcgBinders.Models;
+
+public class Set
+{
+    public int id { get; set; }
+    public string set_name { get; set; }
+    public string set_tag { get; set; }
+    public int card_no { get; set; }
+}
