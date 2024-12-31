46cbcc6ffbc49091f7b0ae1a0c3471b1128c3a12
Kuczek03 <thekolorowyyt@gmail.com>
2024-12-31 01:52:40 +0100

    TcgGames table and view created

---
+++ TcgBinders/Models/TcgGames.cs
@@ -0,0 +1,9 @@
+namespace TcgBinders.Models;
+
+public class TcgGames
+{
+    public int id { get; set; }
+    public string game_name { get; set; }
+    public string game_tag { get; set; }
+    public int no_of_sets { get; set; }
+}
