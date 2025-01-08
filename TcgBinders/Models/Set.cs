a9503893ed55ff31079d258def94b11f36b05968
Kuczek03 <thekolorowyyt@gmail.com>
2025-01-08 17:26:45 +0100

    Scrapping old views, controllers and models etc

--- TcgBinders/Models/Set.cs
+++
@@ -1,9 +0,0 @@
-namespace TcgBinders.Models;
-
-public class Set
-{
-    public int id { get; set; }
-    public string set_name { get; set; }
-    public string set_tag { get; set; }
-    public int card_no { get; set; }
-}
