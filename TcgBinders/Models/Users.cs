8f54fa1f07b912467280c88bdd7af38120a79674
Kuczek03 <thekolorowyyt@gmail.com>
2025-01-08 17:42:12 +0100

    Users table got more details

---
+++ TcgBinders/Models/Users.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TcgBinders.Models;
+
+public class Users
+{
+    [Key]
+    public int id { get; set; }
+    
+    [Required(ErrorMessage = "Username required")]
+    [StringLength(100, MinimumLength = 8)]
+    [Display(Name = "Username")]
+    public string username { get; set; }
+    
+    [Required(ErrorMessage = "Password required")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must have more than 8 characters " +
+                                                         "and use at least one special character")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string password { get; set; }
+    
+    [Required(ErrorMessage = "Password confirmation required")]
+    [StringLength(100, MinimumLength = 8)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("password", ErrorMessage = "Passwords do not match")]
+    public string confirm_password { get; set; }
+    
+    [Required(ErrorMessage = "Email address required")]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string email { get; set; }
+    
+    [Required(ErrorMessage = "Creation date required")]
+    [DataType(DataType.Date)]
+    [Display(Name = "Account creation date")]
+    public DateTime created_at { get; set; }
+    
+    [DataType(DataType.Date)]
+    [Display(Name = "Accounts last update")]
+    public DateTime updated_at { get; set; }
+    
+    [Display(Name = "Status")]
+    public int is_active { get; set; }
+    
+    [Display(Name = "Role")]
+    public int user_role { get; set; }
+}
