771c5b3d7deede83519117921b2ce4acfd11986d
Kuczek03 <thekolorowyyt@gmail.com>
2025-01-09 00:37:42 +0100

    Added unique attribute

--- TcgBinders/Data/BindersContext.cs
+++ TcgBinders/Data/BindersContext.cs
@@ -7,6 +7,19 @@ public class BindersContext : DbContext
 {
     public BindersContext(DbContextOptions<BindersContext> options) : base(options)
     {
+        
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cards>().Property(m => m.image).IsRequired(false);  
+        modelBuilder.Entity<Cards>().Property(m => m.description).IsRequired(false);  
+        
+        modelBuilder.Entity<Sets>().Property(m => m.image).IsRequired(false);  
+        
+        modelBuilder.Entity<Games>().Property(m => m.image).IsRequired(false);  
+        modelBuilder.Entity<Games>().Property(m => m.description).IsRequired(false);   
+        
+        base.OnModelCreating(modelBuilder);
     }
 
     public DbSet<Users> Users { get; set; }
