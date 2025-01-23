399bf5d1507f3e6480fcd8adc2aadb5ee8dee96e
Wiktor Kuczek <117631329+Kuczek03@users.noreply.github.com>
2025-01-23 18:56:19 +0100

    User table created

---
+++ TcgBindersReloaded/Entities/User.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace TcgBindersReloaded.Entities;
+
+[Index(nameof(Username), IsUnique = true)]
+[Index(nameof(Email), IsUnique = true)]
+
+public class User
+{
+    [Key] public int Id { get; set; }
+
+    [Required(ErrorMessage = "Username required")]
+    [StringLength(100, MinimumLength = 4)]
+    [Display(Name = "Username")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "Password required")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must have more than 8 characters " +
+                                                         "and use at least one special character")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Email address required")]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Creation date required")]
+    [DataType(DataType.Date)]
+    [Display(Name = "Account creation date")]
+    public DateTime CreationDate { get; set; }
+}
