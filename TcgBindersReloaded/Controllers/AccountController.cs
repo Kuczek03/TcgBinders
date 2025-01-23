49ffc9d4bad8b0a339e01e329a2f00979cf13055
Wiktor Kuczek <117631329+Kuczek03@users.noreply.github.com>
2025-01-23 18:58:26 +0100

    Account controller handling logging in registering

---
+++ TcgBindersReloaded/Controllers/AccountController.cs
@@ -0,0 +1,112 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TcgBindersReloaded.Entities;
+using TcgBindersReloaded.Models;
+
+namespace TcgBindersReloaded.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly BindersContext _context;
+
+    public AccountController(BindersContext bindersContext)
+    {
+        _context = bindersContext;
+    }
+    
+    // GET
+    public IActionResult Users()
+    {
+        return View(_context.Users.ToList());
+    }
+    
+    // GET
+    public IActionResult Register()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            User account = new User();
+            account.Email = model.Email;
+            account.Username = model.Username;
+            account.Password = model.Password;
+            account.CreationDate = DateTime.Now;
+
+            try
+            {
+                _context.Users.Add(account);
+                _context.SaveChanges();
+
+                ModelState.Clear();
+                ViewData["Message"] = "Account created successfully, please login.";
+            }
+            catch (DbUpdateException e)
+            {
+                ModelState.AddModelError("", "Username, email or password already exists.");
+                return View(model);
+            }
+            return View();
+        }
+        return View(model);
+    }
+    
+    // GET
+    public IActionResult Login()
+    {
+        return View();
+    }
+    
+    // POST
+    [HttpPost]
+    public IActionResult Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = _context.Users.Where(x => (x.Username == model.UsernameEmail || 
+                                                  x.Email == model.UsernameEmail) && x.Password == model.Password)
+                .FirstOrDefault();
+            
+            if (user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Name, user.Username),
+                    new Claim(ClaimTypes.Role, "User"),
+                };
+                
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                
+                return RedirectToAction("Users");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid username/email or password.");
+            }
+        }
+        return View(model);
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Index", "Home");
+    }
+    
+    [Authorize]
+    public IActionResult SecurePage()
+    {
+        ViewBag.Name = HttpContext.User.Identity.Name;
+        return View();
+    }
+}
