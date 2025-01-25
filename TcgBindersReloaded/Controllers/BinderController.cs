7f61918d0b14a759582aadae3168a6580668a7db
Wiktor Kuczek <117631329+Kuczek03@users.noreply.github.com>
2025-01-25 18:58:43 +0100

    Added func to create new binders

--- TcgBindersReloaded/Controllers/BinderController.cs
+++ TcgBindersReloaded/Controllers/BinderController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TcgBindersReloaded.Entities;
+using TcgBindersReloaded.Models;
 
 namespace TcgBindersReloaded.Controllers;
 
@@ -16,7 +17,7 @@ public class BinderController : Controller
     }
 
     // GET
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> BinderCards()
     {
         var user = HttpContext.User.Identity.Name;
         var binder = _context.Binders
@@ -31,6 +32,25 @@ public class BinderController : Controller
         else
         {
             return View(binder.BCards);
+        }
+    }
+    
+    // GET
+    public async Task<IActionResult> UserCards()
+    {
+        var user = HttpContext.User.Identity.Name;
+        var binder = _context.CollectionCards
+            .Include(p => p.User)
+            .Include(p => p.Card)
+            .FirstOrDefault(p => p.User.Username == user);
+
+        if (binder == null)
+        {
+            return View(new List<Card>());
+        }
+        else
+        {
+            return View(binder.Card);
         }
     }
     
@@ -50,5 +70,49 @@ public class BinderController : Controller
         {
             return View(binder.Binders);
         }
+    }
+
+    
+    // GET
+    public IActionResult BinderCreate()
+    {
+        return View();
+    }
+    
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> BinderCreate(BinderViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = HttpContext.User.Identity.Name;
+            var userId = _context.Users.FirstOrDefault(u => u.Username == user)?.Id;
+        
+            Binder binder = new Binder();
+            binder.UserId = userId.Value;
+            binder.Name = model.Name;
+        
+            if (userId == null)
+            {
+                ModelState.AddModelError("", "User not found");
+                return View(model);
+            }
+            
+            try
+            {
+                _context.Add(binder);
+                _context.SaveChangesAsync();
+                ModelState.Clear();
+            }
+            catch (DbUpdateException e)
+            {
+                Console.WriteLine(e);
+                ModelState.AddModelError("", "Cannot create new binder");
+                return View(model);
+            }
+            return RedirectToAction("BinderList");
+        }
+        return View(model);
     }
 }
